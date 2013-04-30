ef870724bf53dc5985075540a2f93e729b7272e6
Vasu Gupta <meetvasu15@gmail.com>
2013-04-30 15:48:36 -0700

    check log file and move to logs dir

--- scripts/rolloverLogs.bat
+++ scripts/rolloverLogs.bat
@@ -1,4 +1,32 @@
-for /f "tokens=1-5 delims=/ " %%d in ("%date%") do rename "asthmaMonitoringLog.txt" asthmaMonitoringLog_%%e-%%f-%%g.txt
-type NUL > asthmaMonitoringLog.txt
-for /f "tokens=1-5 delims=/ " %%d in ("%date%") do rename "SpirometerReadingLog.txt" SpirometerReadingLog_%%e-%%f-%%g.txt
-type NUL > SpirometerReadingLog.txt
+@ECHO off
+set "dir=H:\aspiradeployment\aspiradeploy\configfiles"
+set "file=%dir%\asthmaMonitoringLog.txt"
+
+call :CheckEmpty "%file%" 
+set "file1=%dir%\SpirometerReadingLog.txt"
+call :CheckEmpty1 "%file1%" 
+GOTO:EOF
+
+:CheckEmpty
+if "%~z1" == "" ( 
+    echo log File doesnt exist. 
+) else if "%~z1" == "0" ( 
+    echo log File is empty
+) else ( 
+   for /f "tokens=1-5 delims=/ " %%d in ("%date%") do rename "asthmaMonitoringLog.txt" asthmaMonitoringLog_%%e-%%f-%%g.txt
+   for /f "tokens=1-5 delims=/ " %%d in ("%date%") do move asthmaMonitoringLog_%%e-%%f-%%g.txt logs\
+	type NUL > asthmaMonitoringLog.txt
+)  
+GOTO:EOF
+
+:CheckEmpty1
+if "%~z1" == "" ( 
+    echo spiro File doesnt exist. 
+) else if "%~z1" == "0" ( 
+    echo spiro File is empty
+) else ( 
+	for /f "tokens=1-5 delims=/ " %%d in ("%date%") do rename "SpirometerReadingLog.txt" SpirometerReadingLog_%%e-%%f-%%g.txt
+	for /f "tokens=1-5 delims=/ " %%d in ("%date%") do move SpirometerReadingLog_%%e-%%f-%%g.txt %dir%\logs\
+	type NUL > SpirometerReadingLog.txt
+)  
+GOTO:EOF
