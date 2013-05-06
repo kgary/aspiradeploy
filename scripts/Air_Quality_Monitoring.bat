6ef4fba8aad163629a78f5a1a4a0344c356600c6
Kevin Gary <kgary@asu.edu>
2013-05-05 23:32:39 -0700

    Aspira build as of Sunday May 5 11pm

--- scripts/Air_Quality_Monitoring.bat
+++ scripts/Air_Quality_Monitoring.bat
@@ -1,3 +1,3 @@
 set ASPIRA_HOME=\Users\$$User$$\Documents\
 cd \usr\aspira
-java -cp classes;lib/derby.jar;lib/json-simple-1.1.1.jar edu.asupoly.aspira.Aspira
+java -cp classes;lib/derby.jar;lib/json-simple-1.1.1.jar;lib/jssc25.jar -Djava.library.path=\usr\aspira\lib edu.asupoly.aspira.Aspira
