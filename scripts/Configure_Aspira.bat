cbae00a3ca257d1c2e1547ffd21a6e2d39ba09cb
Kevin Gary <kgary@asu.edu>
2013-05-06 23:48:03 -0700

    Script to start up the config app

---
+++ scripts/Configure_Aspira.bat
@@ -0,0 +1,3 @@
+set ASPIRA_HOME=\Users\$$USER$$\Documents\
+cd \usr\aspira
+java -cp classes;lib/derby.jar;lib/json-simple-1.1.1.jar;lib/poi-ooxml-3.9-20121203.jar;lib/poi-3.9-20121203.jar edu.asupoly.aspira.gui.AdminConfigWindow
